2c634fe3d273b9c49562e05c45f84bbc8d95dc7a
raitako-1 <raiji.kure551.naitoki@gmail.com>
2026-06-29 06:54:42 +0900

    fix cmd

--- src/util/cmd.ts
+++ src/util/cmd.ts
@@ -10,7 +10,7 @@ export const setupCmd = (server: FeedGenerator, logger: Logger) => {
     for (let i = cmd.length; i > 0; i--) {
       const fullPath = path.join(__dirname, `../cmds/${cmd.slice(0, i).join('/')}${path.parse(__filename).ext}`)
       if (fs.existsSync(fullPath)) {
-        const event = (await import(fullPath)).default as (server: FeedGenerator, logger: Logger, args: string[]) => Promise<void>
+        const event = (await import(fullPath)).default.default as (server: FeedGenerator, logger: Logger, args: string[]) => Promise<void>
         await event(server, createLogger(['Runner', 'Commander'].concat(cmd.slice(0, i))), cmd.slice(i))
         return
       }
